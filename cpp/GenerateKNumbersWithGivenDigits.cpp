cb91c33c490d88ce0d2d4a8c88930b0581624d90
tejasmorajkar <tmorajkar316@gmail.com>
2024-01-02 00:10:39 +0530

    dsa commit

--- cpp/GenerateKNumbersWithGivenDigits.cpp
+++ cpp/GenerateKNumbersWithGivenDigits.cpp
@@ -13,6 +13,6 @@ int main() {
 	int k = 10; // 1 2 3 11 12 13 21 22 23 31
 	Solution s;
 	// given 3 digits 1,2,3 generate K numbers formed using these digits
-	s.solve(int k);
+	s.solve(k);
 	return 0;
 }
